edcecd893ae2b2cd0b0317188f5ce1666e9e6cf8
Tuan <trinhsytuan@gmail.com>
2023-09-01 08:47:26 +0700

    add frontend middleware

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -1,17 +1,18 @@
 /* eslint-disable global-require */
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (app, options) => {
-  const isProd = process.env.NODE_ENV === 'production';
+  const isProd = process.env.NODE_ENV === "production";
 
   if (isProd) {
-    const addProdMiddlewares = require('./addProdMiddlewares');
+    app.use(corsMiddlewares);
+    const addProdMiddlewares = require("./addProdMiddlewares");
     addProdMiddlewares(app, options);
   } else {
     app.use(
       "/api",
       createProxyMiddleware({
-        target: "http://localhost:3000",
+        target: "https://apind.sytuan.net:3000",
         changeOrigin: true,
         secure: false,
       })
@@ -22,12 +23,34 @@ module.exports = (app, options) => {
     //     secure: false,
     //     ws: true,
     //   }));
-    const webpackConfig = require('../../webpack/webpack.dev.babel');
-    const addDevMiddlewares = require('./addDevMiddlewares');
+    const webpackConfig = require("../../webpack/webpack.dev.babel");
+    const addDevMiddlewares = require("./addDevMiddlewares");
     addDevMiddlewares(app, webpackConfig);
   }
 
   return app;
 };
 
+function corsMiddlewares(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "https://apind.sytuan.net:3000/");
+
+  // Request methods you wish to allow
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+}
 
